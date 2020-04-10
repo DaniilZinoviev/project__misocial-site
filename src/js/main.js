d0704918bf04bc92d5c32208132f3d4bf57c3298
dzinoviev <danzino21@gmail.com>
2020-04-10 09:38:57 +0300

    some work

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@
  //= ../../bower_components/jquery/dist/jquery.min.js
  //= ../../bower_components/bootstrap/dist/js/bootstrap.min.js
  //= ../../bower_components/animatescroll/animatescroll.min.js
+ //= ../../bower_components/slick-carousel/slick/slick.min.js
 
 /**
  * Custom
--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-	// Dropdown button
+	/**
+	 * Dropdown button
+	 */
 	$('.menu-btn').on('click', function() {
 		var $this = $(this);
 		$this.toggleClass('close');
@@ -12,4 +14,22 @@ $(document).ready(function() {
 		$(href).animatescroll();
 	});
 
+	/**
+	 * Sliders
+	 */
+	$('.homepage .section-sixth .slider').slick({
+		dots: true,
+		initialSlide: 1,
+		arrows: false,
+		draggable: false,
+		slidesToShow: 2,
+		responsive: [
+	    {
+	      breakpoint: 992,
+	      settings: {
+	        slidesToShow: 1,
+	      }
+	    },
+    ]
+	});
 });
